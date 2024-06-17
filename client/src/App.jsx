5d1f6a69a3d3655e7e3dae14a613fbc6af42c0ba
Stanimir Reshavski <mchorko@gmail.com>
2024-06-17 16:44:36 +0300

    Screen Saver screen updated and created styles folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import ScreenSaver from "./components/screens/ScreenSaver/ScreenSaver";
+import "../styles/style.css";
 
 const App = () => {
     return (
--- client/src/components/screens/ScreenSaver/ScreenSaver.jsx
+++ client/src/components/screens/ScreenSaver/ScreenSaver.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./ScreenSaver.css";
 
 const ScreenSaver = () => {
-  return (
-    <div className="screen-saver">
-      <h1>Screen Saver</h1>
-    </div>
-  );
+    return (
+        <div className="screen-saver">
+            <article className="purchase-price-board">
+                <p>Купи:</p>
+                <ul className="prices">
+                    <li>
+                        <span>1 BTC = </span> 100000 BGN
+                    </li>
+                    <li>
+                        <span>1 ETH = </span> 9000 BGN
+                    </li>
+                </ul>
+            </article>
+            <article className="screen-saver-container">
+                <img src="vite.svg" />
+                <div className="screen-saver-heading">
+                    <h1>Купи Биткойни</h1>
+                    <p>Докоснете екрана за да започнете</p>
+                </div>
+            </article>
+        </div>
+    );
 };
 
 export default ScreenSaver;
