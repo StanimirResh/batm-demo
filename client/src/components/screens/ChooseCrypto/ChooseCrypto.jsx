8a9fb55d8feb6f45cc1c67318a6871c347be7d0e
Stanimir Reshavski <mchorko@gmail.com>
2024-06-17 21:03:26 +0300

    Choose crypto screen finished style

--- client/src/components/screens/ChooseCrypto/ChooseCrypto.jsx
+++ client/src/components/screens/ChooseCrypto/ChooseCrypto.jsx
@@ -4,11 +4,65 @@ import styles from "./ChooseCrypto.module.css";
 const ChooseCrypto = () => {
     return (
         <div className={styles.chooseCryptoScreen}>
-            <h1>Choose cryptocurrency</h1>
-            <p>BTC</p>
-            <p>ETH</p>
-            <p>LTC</p>
-            <p>USDT</p>
+            <section className={styles.languageContainer}>
+                <img src="bulgaria-flag.svg" alt="" />
+                <img src="uk-flag.svg" alt="" />
+            </section>
+            <article className={styles.chooseCryptoWrapper}>
+                <section className={styles.chooseCryptoHeadingContainer}>
+                    <img
+                        src="btc-logo.svg"
+                        alt="btc-logo"
+                        className={styles.logo}
+                    />
+                    <div className={styles.chooseCryptoHeading}>
+                        <p>Добре дошли на нашия Биткойн АТМ!</p>
+                        <h3>Купи Биткойни!</h3>
+                    </div>
+                </section>
+
+                <section className={styles.supportedCoins}>
+                    <div>
+                        <img
+                            src="btc-logo.svg"
+                            alt=""
+                            className={styles.coinImg}
+                        />
+                        <p>1 BTC</p>
+                        <p>100000 BGN</p>
+                    </div>
+                    <div>
+                        <img
+                            src="eth-logo.svg"
+                            alt=""
+                            className={styles.coinImg}
+                        />
+                        <p>1 ETH</p>
+                        <p>10000 BGN</p>
+                    </div>
+                    <div>
+                        <img
+                            src="ltc-logo.svg"
+                            alt=""
+                            className={styles.coinImg}
+                        />
+                        <p>1 LTC</p>
+                        <p>300 BGN</p>
+                    </div>
+                    <div>
+                        <img
+                            src="usdt-logo.svg"
+                            alt=""
+                            className={styles.coinImg}
+                        />
+                        <p>1 USDT</p>
+                        <p>2 BGN</p>
+                    </div>
+                </section>
+            </article>
+            <section className={styles.chooseCryptoHelp}>
+                <p>За да продължите докоснете съответната монетна икона</p>
+            </section>
         </div>
     );
 };
