a44234def214f43a6124656b39af23da2f5d7a1d
Stanimir Reshavski <mchorko@gmail.com>
2024-06-21 19:15:02 +0300

    Created common component for the header

---
+++ client/src/components/common/MenusHeader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./MenusHeader.module.css"
+
+const MenusHeader = () => {
+    return (
+        <>
+            <section className={styles.languageContainer}>
+                <img src="bulgaria-flag.svg" alt="" />
+                <img src="uk-flag.svg" alt="" />
+            </section>
+            <section className={styles.chooseCryptoHeadingContainer}>
+                <img
+                    src="btc-logo.svg"
+                    alt="btc-logo"
+                    className={styles.logo}
+                />
+                <div className={styles.chooseCryptoHeading}>
+                    <p>Добре дошли на нашия Биткойн АТМ!</p>
+                    <h3>Купи Биткойни!</h3>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default MenusHeader;
