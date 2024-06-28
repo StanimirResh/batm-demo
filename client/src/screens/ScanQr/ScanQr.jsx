88588887d1982bff005fe631dddefbcc27967c17
Stanimir Reshavski <mchorko@gmail.com>
2024-06-28 17:16:05 +0300

    Adjusted QR screen look

--- client/src/screens/ScanQr/ScanQr.jsx
+++ client/src/screens/ScanQr/ScanQr.jsx
@@ -3,26 +3,27 @@ import styles from "./ScanQr.module.css";
 
 import { AppContext } from "../../contexts/AppContext";
 import CancelButton from "../../components/common/CancelButton/CancelButton";
+import HelpText from "../../components/common/HelpText/HelpText";
 
 const ScanQr = () => {
     const { coin, action } = useContext(AppContext);
 
     return (
         <div className={styles.scanQrScreen}>
-            <div>
-                <img src="btc-logo.svg" alt="" />
-            </div>
             <div className={styles.qrHeadingContainer}>
                 <h1>Въведете {coin.symbol} адрес за получаване</h1>
                 <p>Сканирайте QR кода на Вашия портфейл</p>
             </div>
             <div className={styles.qrRightMenu}>
-                <CancelButton sticky={false}/>
                 <div className={styles.qrPreviewContainer}>
                     <p>Преглед</p>
                     <div className={styles.qrPreviewWindow}></div>
                 </div>
+                <HelpText
+                    text={`Адресът за получуване е адрес от Вашия портфейл, където закупените биткойни ще бъдат изпратени.\nУверете се, че е правилен!`}
+                />
             </div>
+            <CancelButton sticky={true} />
         </div>
     );
 };
