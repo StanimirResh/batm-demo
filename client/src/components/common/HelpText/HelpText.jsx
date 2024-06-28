5f02fe95fe4552f91265c577505e9b522f9fb955
Stanimir Reshavski <mchorko@gmail.com>
2024-06-28 17:18:23 +0300

    Removed unnecessary lines

--- client/src/components/common/HelpText/HelpText.jsx
+++ client/src/components/common/HelpText/HelpText.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router";
 import styles from "./HelpText.module.css";
 
 const HelpText = (props) => {
-    const navigate = useNavigate();
-
     return (
         <section className={styles.chooseCryptoHelp}>
             <p>{props.text}</p>
