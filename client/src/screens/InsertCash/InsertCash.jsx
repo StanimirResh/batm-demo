803740198d5f06bf2b4eb56e72b2da5f80e41149
Stanimir Reshavski <mchorko@gmail.com>
2024-06-28 20:29:03 +0300

    More styling to the Insert cash screen

--- client/src/screens/InsertCash/InsertCash.jsx
+++ client/src/screens/InsertCash/InsertCash.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router";
 import { AppContext } from "../../contexts/AppContext";
-import styles from "./InsertCash.module.css"
+import CancelButton from "../../components/common/CancelButton/CancelButton";
+
+import styles from "./InsertCash.module.css";
 
 const InsertCash = () => {
     const { coin, action } = useContext(AppContext);
@@ -18,8 +20,18 @@ const InsertCash = () => {
     }
 
     return (
-        <div>
-            <h1>Insert cash</h1>
+        <div className={styles.insertCashScreen}>
+            <div className={styles.insertHeadingContainer}>
+                <h1>Подайте банкноти</h1>
+                <p>
+                    Моля, поставете банкнотите, които искате да обмените за{" "}
+                    {coin.symbol}
+                </p>
+            </div>
+            <div>
+                
+            </div>
+            <CancelButton sticky={true} />
         </div>
     );
 };
